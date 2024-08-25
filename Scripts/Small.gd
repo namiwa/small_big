098e3b8b9d734a5fbc82af7455945b0cc41e9d94
namiwa <khairul.nasri895@gmail.com>
2024-08-26 00:59:34 +0800

    feat: getting motions to be smooth

--- Scripts/Small.gd
+++ Scripts/Small.gd
@@ -5,7 +5,6 @@ signal hit
 @export var TYPE = "small"
 @export var SPEED = 200
 @export var JUMP_VELOCITY = -400.0
-var CAN_JUMP = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,29 +12,24 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func get_type():
 	return TYPE
 
-
-func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	
-	# Handle Jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		CAN_JUMP = true
-	
-	if CAN_JUMP and not is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		CAN_JUMP = false
-		
+func _physics_process(delta):			
 	var direction = Input.get_axis("Left", "Right")
+	
+	if not is_on_floor():
+		# Handle Gravity
+		velocity.y += gravity * delta
 
 	if is_on_floor():
 		if direction > 0:
-			$AnimatedSprite2D.animation = "right"
-		elif direction < 0:
 			$AnimatedSprite2D.animation = "left"
+		elif direction < 0:
+			$AnimatedSprite2D.animation = "right"
 		else:
 			$AnimatedSprite2D.animation = "default"
+			
+		if Input.is_action_just_pressed("Jump"):
+			velocity.y = JUMP_VELOCITY
+
 	else:
 		$AnimatedSprite2D.animation = "jump"
 	
